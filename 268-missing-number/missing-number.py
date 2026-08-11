65656fae82ed68faf7d84b5090806cbdbd214596
Arya <aryamore2020@gmail.com>
2026-08-11 12:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (36.30%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def missingNumber(self, nums):
-        n = len(nums)
+        xor = len(nums)
 
-        xor = 0
+        
 
-        for i in range(n + 1):
+        for i in range(len(nums)):
             xor ^= i
-
-        for num in nums:
-            xor ^= num
+            xor ^= nums[i]
+        
+            
 
         return xor
